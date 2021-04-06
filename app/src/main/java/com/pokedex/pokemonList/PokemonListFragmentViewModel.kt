b47622fe7796b82cf1a4f8331f277a95c6212787
Aldo Pedraza <aldpdz@gmail.com>
2021-04-06 14:58:40 -0400

    Adding operators

--- app/src/main/java/com/pokedex/pokemonList/PokemonListFragmentViewModel.kt
+++ app/src/main/java/com/pokedex/pokemonList/PokemonListFragmentViewModel.kt
@@ -2,13 +2,14 @@ package com.pokedex.pokemonList
 
 import androidx.lifecycle.*
 import com.pokedex.data.remote.BasicInfo
-import com.pokedex.data.remote.Type
 import com.pokedex.repositories.IPokemonRepository
 import com.pokedex.utils.RemoteStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,7 +33,7 @@ class PokemonListFragmentViewModel @Inject constructor(
         getPokemons()
     }
 
-    private fun getPokemons() {
+    fun getPokemons(filter: String? = null) {
         _networkStatus.value = RemoteStatus.LOADING
         compositeDisposable.add(
                 pokemonRepository.retrivePokemons()
@@ -40,30 +41,20 @@ class PokemonListFragmentViewModel @Inject constructor(
                         .map { content ->
                             content.body()?.let {
                                 val list = it.results
-                                for (pokemon in list) {
-                                    val url = pokemon.url
-                                    val pokemonIndex = url.split("/")
-                                    pokemon.urlImg = POKEMON_IMAGES_ENDPOINT
-                                            .plus(pokemonIndex[pokemonIndex.size - 2])
-                                            .plus(".png")
-
-                                    pokemonRepository.getDetails(pokemon.url)
-                                        .subscribeOn(Schedulers.io())
-                                        .subscribe{ response ->
-                                            response.body()?.let { detail ->
-                                                var typeString = "-"
-                                                for(type in detail.types){
-                                                    typeString = typeString
-                                                        .plus(type.type.name)
-                                                        .plus("-")
-                                                }
-                                                pokemon.types = typeString
-                                            }
-                                        }
-                                }
+                                getImageUrl(list)
 
                                 return@map list
                             }
+                        }
+                        .map {
+                            val filterList = mutableListOf<BasicInfo>()
+                            for (poke in it){
+                                if(filter == null || poke.name.contains(filter, ignoreCase = true)
+                                        || filter.isEmpty()){
+                                    filterList.add(poke)
+                                }
+                            }
+                            return@map filterList
                         }
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe (
@@ -73,6 +64,81 @@ class PokemonListFragmentViewModel @Inject constructor(
                             {
                                 _networkStatus.value = RemoteStatus.ERROR })
         )
+    }
+
+    private fun getDetails(pokemon: BasicInfo) {
+        compositeDisposable.add(
+                pokemonRepository.getDetails(pokemon.url)
+                        .subscribeOn(Schedulers.io())
+                        .subscribe ({ response ->
+                            response.body()?.let { detail ->
+                                var typeString = "-"
+                                for (type in detail.types) {
+                                    typeString = typeString
+                                            .plus(type.type.name)
+                                            .plus("-")
+                                }
+                                pokemon.types = typeString
+                            }
+                        }, {
+
+                        })
+        )
+    }
+
+    fun getLast10() {
+        _networkStatus.value = RemoteStatus.LOADING
+        compositeDisposable.add(
+                pokemonRepository.retrivePokemons()
+                        .subscribeOn(Schedulers.io())
+                        .map { content ->
+                            content.body()?.let {
+                                val list = it.results
+                                getImageUrl(list)
+                                return@map list
+                            }
+                        }
+                        .map {
+                            val create = createObservable(it)
+                            create.observeOn(Schedulers.io())
+                        }
+                        .subscribe (
+                                {
+                                    it.subscribeOn(Schedulers.io())
+                                            .takeLast(10)
+                                            .map { basicInfo ->
+                                                Thread.sleep(1000)
+                                                basicInfo
+                                            }
+                                            .observeOn(AndroidSchedulers.mainThread())
+                                            .subscribe{ basicInfo ->
+                                                _networkStatus.value = RemoteStatus.DONE
+                                                _pokemons.value = listOf(basicInfo)
+                                            }
+                                },
+                                {
+                                    _networkStatus.value = RemoteStatus.ERROR }))
+    }
+
+    private fun createObservable(content: List<BasicInfo>) =
+        Observable.create<BasicInfo>{ emmiter ->
+            content.forEach { basicInfo ->
+                emmiter.onNext(basicInfo)
+            }
+            emmiter.onComplete()
+        }
+
+
+    private fun getImageUrl(list: List<BasicInfo>) {
+        for (pokemon in list) {
+            val url = pokemon.url
+            val pokemonIndex = url.split("/")
+            pokemon.urlImg = POKEMON_IMAGES_ENDPOINT
+                    .plus(pokemonIndex[pokemonIndex.size - 2])
+                    .plus(".png")
+
+            getDetails(pokemon)
+        }
     }
 
     override fun onCleared() {
